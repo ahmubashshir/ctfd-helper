aef2249fab8829026ad217e61ee86f359c6342bb
Mubashshir <ahmubashshir@gmail.com>
2025-12-14 01:34:08 +0600

    Use a wrapper to ensure socket is closed in tcpxy
    Use the wrapper to show exit status if DEBUG is set
    
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- src/tcpxy.c
+++ src/tcpxy.c
@@ -4,6 +4,7 @@
  * @debug
  * @-pipe
  * @-pie
+ * @-g
  */
 
 #include <arpa/inet.h>
@@ -18,26 +19,27 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
 #define MAX_CMD_LENGTH 1024
 #define MAX_ARGS 100
 
 int server_fd = -1;
 void handle_sigchld(int sig)
 {
-	while (waitpid(-1, NULL, WNOHANG) > 0) {}
+	while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-void handle_sigint(int sig)
+void handle_sigint_server(int sig)
 {
 	if (server_fd != -1) {
 		close(server_fd);
 	}
+	kill(0, sig);
+	while (waitpid(-1, NULL, 0) > 0);
 	printf("Server shutting down...\n");
 	exit(0);
 }
 
-void start_handler(int fd, char *const args[])
+int start_handler(int fd, char *const args[])
 {
 	// Fork a new process to handle the connection
 
@@ -45,13 +47,19 @@ void start_handler(int fd, char *const args[])
 	if (pid < 0) {
 		perror("fork failed");
 		close(fd);
-		return;
+		return -1;
 	} else if (pid == 0) {
 		// Redirect input/output to the client socket
-		if (dup2(fd, STDIN_FILENO) < 0 || dup2(fd, STDOUT_FILENO) < 0 || dup2(fd, STDERR_FILENO) < 0) {
-			perror("dup2 failed");
-			close(fd);
-			exit(1);
+		switch(fd) {
+		case -1:
+			break;
+		default:
+			if (dup2(fd, STDIN_FILENO) < 0 || dup2(fd, STDOUT_FILENO) < 0) {
+				perror("dup2 failed");
+				close(fd);
+				exit(1);
+			};
+			break;
 		}
 
 		if (execv(args[0], args) < 0) {
@@ -60,13 +68,16 @@ void start_handler(int fd, char *const args[])
 			exit(1);
 		}
 	}
+	return pid;
 }
 
 void print_client_addr(struct sockaddr_in *client_addr)
 {
-	char client_ip[INET_ADDRSTRLEN];
+	char client_ip[INET_ADDRSTRLEN + 8];
 	inet_ntop(AF_INET, &client_addr->sin_addr, client_ip, sizeof(client_ip));
-	printf("Client connected -> %s:%d\n", client_ip, ntohs(client_addr->sin_port));
+	size_t n = strnlen(client_ip, INET_ADDRSTRLEN);
+	snprintf((client_ip + n), 8, ":%d", ntohs(client_addr->sin_port));
+	setenv("CLIENT", client_ip, 1);
 }
 
 // Function to handle the TCP listener and execute the given command
@@ -120,7 +131,7 @@ void start_server(int port, char *const cmdline[])
 	printf("Listening on port %d...\n", port);
 	printf("Handler: {");
 	{
-		size_t i = 0;
+		size_t i = 1;
 		while(cmdline[i] != NULL) {
 			printf("\"%s\"%s", cmdline[i], (cmdline[i + 1]==NULL)?"}\n":", ");
 			i++;
@@ -139,35 +150,126 @@ void start_server(int port, char *const cmdline[])
 
 		print_client_addr(&client_addr);
 		start_handler(client_fd, cmdline);
+		unsetenv("CLIENT");
 		close(client_fd);
 	}
 
 	close(server_fd);
 }
 
-int main(int argc, char *argv[])
+
+int server_main(int argc, char *argv[])
 {
 	if (argc < 2) {
 		fprintf(stderr, "Usage: %s <path> [args ...]\n", argv[0]);
 		exit(1);
 	}
 
-	signal(SIGINT, handle_sigint);
-	signal(SIGTERM, handle_sigint);
+	signal(SIGINT, handle_sigint_server);
+	signal(SIGTERM, handle_sigint_server);
 	signal(SIGCHLD, handle_sigchld);
 
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-	setvbuf(stdin, NULL, _IONBF, 0);
-
 	int port = 20240;
+	char *cmdline[argc + 1];
+	for(size_t i = 0; i < argc; i++) {
+		cmdline[i] = argv[i];
+		cmdline[i + 1] = NULL;
+	}
+	setenv("SERVER", "1", 1);
+	start_server(port, cmdline);
+
+	return 0;
+}
+
+void handle_sigint_handler(int sig)
+{
+	kill(0, sig);
+	while (waitpid(-1, NULL, 0) > 0);
+	fprintf(stderr, "Closing connection -> %s\n", getenv("CLIENT"));
+	shutdown(STDIN_FILENO, SHUT_RDWR);
+	shutdown(STDOUT_FILENO, SHUT_RDWR);
+	exit(0);
+}
+
+void print_abnormal_exit(int sig)
+{
+	const char *signal_name;
+
+	// Determine the signal name based on the signal number
+	switch(sig) {
+	case SIGSEGV:
+		signal_name = "SIGSEGV";
+		break;
+	case SIGFPE:
+		signal_name = "SIGFPE";
+		break;
+	case SIGPIPE:
+		signal_name = "SIGPIPE";
+		break;
+	case SIGABRT:
+		signal_name = "SIGABRT";
+		break;
+	case SIGILL:
+		signal_name = "SIGILL";
+		break;
+	case SIGBUS:
+		signal_name = "SIGBUS";
+		break;
+	default:
+		signal_name = "Unknown Signal";
+		break;
+	}
+
+	// Print the signal name
+	printf("Handler terminated abnormally wtih %s\n", signal_name);
+}
+
+int handler_main(int argc, char *argv[])
+{
+
+	signal(SIGINT, handle_sigint_handler);
+	signal(SIGTERM, handle_sigint_handler);
+	signal(SIGCHLD, handle_sigchld);
 	char *cmdline[argc - 1];
 	for(size_t i = 0; i < (argc - 1); i++) {
 		cmdline[i] = argv[i + 1];
 		cmdline[i + 1] = NULL;
 	}
+	pid_t pid = start_handler(-1, cmdline);
+	int status;
+	pid_t wpid = waitpid(pid, &status, 0);
+	if (wpid == -1) {
+		perror("waitpid failed");
+		exit(1);
+	}
+	if(getenv("DEBUG") != NULL) {
+		if (WIFSIGNALED(status))
+			print_abnormal_exit(WTERMSIG(status));
+		else if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			printf("Handler exited with error code %d\n", WEXITSTATUS(status));
+		else if (! WIFEXITED(status))
+			printf("Handler terminated abnormally (unknown reason)\n");
+	}
 
-	start_server(port, cmdline);
+	printf("Press ENTER to disconnect...\n");
 
+	shutdown(STDIN_FILENO, SHUT_RDWR);
+	shutdown(STDOUT_FILENO, SHUT_RDWR);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
 	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	setvbuf(stdin, NULL, _IONBF, 0);
+
+	if(getenv("SERVER") == NULL) return server_main(argc, argv);
+	else if(getenv("CLIENT") != NULL) return handler_main(argc, argv);
+	else {
+		printf("Invalid invocation.\n");
+		return 1;
+	}
 }
